892f92c48bc90fd114598385f1eb6a64ccbc6f6e
Wladyslaw Rouba <roubovlad@gmail.com>
2025-08-01 16:04:48 +0200

    Add HW6 to App

--- 2-спринт/src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ 2-спринт/src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -6,11 +6,12 @@ import s2 from '../Affairs.module.css'
 type AffairPropsType = {
     // key не нужно типизировать
     affair: AffairType
-    deleteAffairCallback: any // need to fix any
+    deleteAffairCallback: (affairId: number) => void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
     const deleteCallback = () => {
+        props.deleteAffairCallback(props.affair._id)
         // need to fix
     }
 
@@ -24,10 +25,9 @@ function Affair(props: AffairPropsType) {
             className={affairClass}
         >
             <div id={'hw2-name-' + props.affair._id} className={nameClass}>
-                {/*создаёт студент*/}
 
-                {/**/}
-            </div>
+                {props.affair.name}
+</div>
             <div id={'hw2-priority-' + props.affair._id} hidden>
                 {props.affair.priority}
             </div>
@@ -35,12 +35,13 @@ function Affair(props: AffairPropsType) {
             <button
                 id={'hw2-button-delete-' + props.affair._id}
                 className={buttonClass}
+                onClick ={deleteCallback}
                 // need to fix
 
             >
-                {/*текст кнопки могут изменить студенты*/}
+
                 X
-                {/**/}
+
             </button>
         </div>
     )
--- 2-спринт/src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ 2-спринт/src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -42,19 +42,19 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
 
     const onEnterCallback = () => {
         // выключить editMode при нажатии Enter // делают студенты
-
+        setEditMode(false)
         onEnter?.()
     }
     const onBlurCallback = (e: React.FocusEvent<HTMLInputElement>) => {
         // выключить editMode при нажатии за пределами инпута // делают студенты
-
+        setEditMode(false)
         onBlur?.(e)
     }
     const onDoubleClickCallBack = (
         e: React.MouseEvent<HTMLSpanElement, MouseEvent>
     ) => {
         // включить editMode при двойном клике // делают студенты
-
+        setEditMode(true)
         onDoubleClick?.(e)
     }
 
@@ -65,6 +65,7 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
         <>
             {editMode ? (
                 <SuperInputText
+                    id={restProps.id}
                     autoFocus={autoFocus || true}
                     onBlur={onBlurCallback}
                     onEnter={onEnterCallback}
@@ -79,6 +80,7 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
                         alt={'edit'}
                     />
                     <span
+                        id={spanProps?.id}
                         onDoubleClick={onDoubleClickCallBack}
                         className={spanClassName}
                         {...restSpanProps}
